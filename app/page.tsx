ef310aaaab4756cf797d765ab79c6b11ef37c604
Cavor789 <cavor@bk.ru>
2025-10-14 14:26:12 +0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  if (typeof window !== 'undefined') {
-    window.location.replace('/cards'); // отправим посетителя в раздел карточек
-  }
-  return null;
+  redirect('/cards');
 }
