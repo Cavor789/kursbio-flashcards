7ec28aff0f2527703396ff1eaf81042fa69a65ae
Cavor789 <cavor@bk.ru>
2025-10-14 19:29:49 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // весь сайт будет жить под путём /cards
-  basePath: '/cards',
-  reactStrictMode: true,
-  trailingSlash: false,
-  // если используешь next/image с внешними доменами — добавь сюда remotePatterns
-  images: { remotePatterns: [] },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cards',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
