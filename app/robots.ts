55762f387e9180842c6e8d1ebbab0b62c0d4e758
Cavor789 <cavor@bk.ru>
2025-10-16 12:12:57 +0300

    Delete robots.ts

--- app/robots.ts
+++
@@ -1,7 +0,0 @@
-export default function robots() {
-  const BASE = process.env.NEXT_PUBLIC_SITE_URL || 'https://kursbio.ru';
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: `${BASE}/cards/sitemap.xml`,
-  };
-}
