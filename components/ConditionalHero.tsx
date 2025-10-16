d9e05113ff1839ca9ee9f6916f08a8d959cd6cc7
Cavor789 <cavor@bk.ru>
2025-10-16 12:13:22 +0300

    Delete ConditionalHero.tsx

--- components/ConditionalHero.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import type { PropsWithChildren } from 'react';
-
-export default function ConditionalHero({ children }: PropsWithChildren) {
-  const pathname = usePathname();
-  // показываем баннер только на /biology/chemistry
-  return pathname === '/biology/chemistry' ? <>{children}</> : null;
-}
